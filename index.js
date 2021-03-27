07a9a222f555473d86a0fef5f8f9bef29e64c789
WillianFarias <williansousafarias@gmail.com>
2021-03-27 17:47:12 -0300

    post

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
-app.listen(3001, () => {
-  console.log("running on port 3001");
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "will17",
+  database: "crud"
+})
+
+app.post("/api/insert", (req, res) => {
+  const sqlInsert = 
+    "INSERT INTO movie_review (movieName, movieReview) VALUES (?, ?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {});
 });
