6746ed44ce347a53e7a685aa90c6b218bef1a7ea
WillianFarias <williansousafarias@gmail.com>
2021-03-27 22:36:35 -0300

    integrando back e front

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
 const mysql = require('mysql');
 
@@ -7,6 +9,21 @@ const db = mysql.createPool({
   user="root",
   password="will17",
   database="movie_review"
+});
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/api/insert", (req, res) => {
+
+  const movieName = req.body.movieName
+  const movieReview = req.body.movieReview
+
+  const sqlInsert = "INSERT INTO movie_review (movieName, movieReview) VALUES (?, ?)"
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+    console.log(err);
+  });
 });
 
 app.listen(3001, () => {
